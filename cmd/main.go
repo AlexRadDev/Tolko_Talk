db0f267a0b46e5de46ca9395649d6351fa36bfbb
AlexRad <AlexRadDev@gmail.com>
2025-05-14 16:27:59 +0300

    Добавил файлы: mod, .env, main переписал, добавил извлечение переменных среды

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,59 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+	"log/slog"
 	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"tolko_talk/internal/app_telega"
+)
+
+const (
+	pathEnv  = "D:/go_progect_for_Git/tolko_talk/.env"
+	timeNews = 20 // промежуток времени в минутах (за который нужно скачать новости)
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	log.Printf("Open http://localhost:%s in the browser", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	_, err := fmt.Fprint(w, "Hello, World!")
+	// Загружаем файл .env
+	err := godotenv.Load(pathEnv)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Fatalf("Ошибка загрузки .env файла: %v", err)
 	}
+	slog.Info("Прочитали файл .env")
+
+	// Получаем переменные окружения
+	apiIDStr := os.Getenv("API_ID")
+	if apiIDStr == "" {
+		log.Fatal("Ошибка: API_ID не задан в .env")
+	}
+	apiID, err := strconv.Atoi(apiIDStr)
+	if err != nil {
+		log.Fatalf("Ошибка при переводе API_ID в int: %v", err)
+	}
+	apiHash := os.Getenv("API_HASH")
+	if apiHash == "" {
+		log.Fatal("Ошибка: API_HASH не задан в .env")
+	}
+	channelName := os.Getenv("CHANNEL_NAME")
+	if channelName == "" {
+		log.Fatal("Ошибка: CHANNEL_USERNAME не задан в .env")
+	}
+	if len(channelName) > 0 && channelName[0] == '@' { // Убираем @ из имени канала
+		channelName = channelName[1:]
+	}
+	phone := os.Getenv("PHONE")
+	if phone == "" {
+		log.Fatal("Ошибка: PHONE не задан в .env")
+	}
+	password := os.Getenv("TWO_FACTOR_AUTH")
+	if password == "" {
+		log.Fatal("Ошибка: TWO_FACTOR_AUTH не задан в .env")
+	}
+	//slog.Info(fmt.Sprintf("Мой TWO_FACTOR_AUTH из .env: %s", twoFactorAuth))
+	slog.Info("Создали переменные API данных")
+
+	// Запускаем парсинг канала телеги
+	app_telega.RunTelegaApp(apiID, apiHash, channelName, phone, password, timeNews)
 }
